c4870ae9f01d344a1ab72703eb52ee28256b5bbc
yaqoob6666 <140539701+yaqoob6666@users.noreply.github.com>
2023-07-27 13:16:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -34,13 +34,13 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "YAQOOB SK"    
+global.ownernumber = ["923072984786"]
 global.ytname = "YT: Enter Your Channel Name Here"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
+global.region = "Pakistan"
 global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.timezone = "Asia/Lahore" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
 global.email = "Enter Your Email ID Here"
 
